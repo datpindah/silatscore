2e5563f4eec3da089375b5824b12275d8b4cb233
Pindah Dat <datpindah@gmail.com>
2025-06-20 15:55:28 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -1,3 +1,4 @@
+
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
@@ -5,13 +6,15 @@ import { cn } from "@/lib/utils"
 const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
->(({ className, ...props }, ref) => (
+>(({ className, children, ...rest }, ref) => (
   <div className="relative w-full overflow-auto">
     <table
       ref={ref}
       className={cn("w-full caption-bottom text-sm", className)}
-      {...props}
-    />
+      {...rest}
+    >
+      {children}
+    </table>
   </div>
 ))
 Table.displayName = "Table"
